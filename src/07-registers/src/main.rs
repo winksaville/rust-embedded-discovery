305989305ef0056e9d474528496b87ccb4e1ebac
Wink Saville <wink@saville.com>
2020-12-26 17:56:57 -0800

    Add BAAAAAAD example

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,6 +1,36 @@
 #![no_main]
 #![no_std]
 
+//use core::ptr;
+//
+//#[allow(unused_imports)]
+//use aux7::{entry, iprint, iprintln};
+//
+//#[entry]
+//fn main() -> ! {
+//    aux7::init();
+//
+//    unsafe {
+//        // A magic address!
+//        const GPIOE_BSRR: u32 = 0x48001018;
+//
+//        // Turn on the "North" LED (red)
+//        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+//
+//        // Turn on the "East" LED (green)
+//        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+//
+//        // Turn off the "North" LED
+//        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+//
+//        // Turn off the "East" LED
+//        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+//    }
+//
+//    loop {}
+//}
+ 
+// 0xBAAAAAAD address
 use core::ptr;
 
 #[allow(unused_imports)]
@@ -11,20 +41,7 @@ fn main() -> ! {
     aux7::init();
 
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
-
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
-
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        ptr::read_volatile(0x4800_1800 as *const u32);
     }
 
     loop {}
