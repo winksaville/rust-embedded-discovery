92c1ce5ea20d0ed753471d1b8aef77e05ba1774d
Wink Saville <wink@saville.com>
2020-12-28 14:46:01 -0800

    Leds again
    First I changed lib.rs to expose ITM and iprintln. I then read the manual
    and used a rcc.ahbenr.write to change ahbenr. I then looked at
    "the-solution" and saw I should use modify not write. I couldn't figure
    out how to configure the GPIOE to output until I saw the solution.
    
    I then went back and wanted to see if using rcc.ahbenr.write would
    work and it does, but noticed that it "obviously" only set bit 21. So I
    added read_ahbenr so I could "or" the current values and set bit 21.

--- src/08-leds-again/auxiliary/src/lib.rs
+++ src/08-leds-again/auxiliary/src/lib.rs
@@ -5,15 +5,17 @@
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate panic_itm; // panic handler
 
-pub use cortex_m::asm::bkpt;
+pub use cortex_m::{asm::bkpt, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
 pub use f3::hal::stm32f30x::{gpioc, rcc};
 
 use f3::hal::stm32f30x::{self, GPIOE, RCC};
 
-pub fn init() -> (&'static gpioc::RegisterBlock, &'static rcc::RegisterBlock) {
+pub fn init() -> (ITM, &'static gpioc::RegisterBlock, &'static rcc::RegisterBlock) {
     // restrict access to the other peripherals
     (stm32f30x::Peripherals::take().unwrap());
 
-    unsafe { (&*GPIOE::ptr(), &*RCC::ptr()) }
+    let p = cortex_m::Peripherals::take().unwrap();
+
+    unsafe { (p.ITM, &*GPIOE::ptr(), &*RCC::ptr()) }
 }
--- src/08-leds-again/src/main.rs
+++ src/08-leds-again/src/main.rs
@@ -1,14 +1,49 @@
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-use aux8::entry;
+use core::ptr;
+use aux8::{entry, iprintln};
+
+fn read_ahbenr() -> u32 {
+    const RCC: u32 = 0x4002_1000 + 0x14;
+    unsafe { ptr::read_volatile(RCC as *const u32) }
+}
 
 #[entry]
 fn main() -> ! {
-    let (gpioe, rcc) = aux8::init();
+    let (mut itm, gpioe, rcc) = aux8::init();
 
     // TODO initialize GPIOE
+    iprintln!(&mut itm.stim[0], "1 read ahbenr={:#0x}", read_ahbenr());
+
+    //// My original solution, which requires commenting out deny(unsafe_code)
+    //rcc.ahbenr.write(|w| {
+    //    unsafe { w.bits(1 << 21) }
+    //});
+
+    //// My modified solution which reads original value and then sets bit 21
+    //rcc.ahbenr.write(|w| {
+    //    unsafe { w.bits(read_ahbenr() | (1 << 21)) }
+    //});
+
+    // The "proper" solution which uses modify and sets bit iopen (i.e. 21)
+    rcc.ahbenr.modify(|_, w| {
+        w.iopeen().set_bit()
+    });
+    iprintln!(&mut itm.stim[0], "2 read ahbenr={:#0x}", read_ahbenr());
+
+    // From the-solution.md, I didn't figure out :)
+    gpioe.moder.modify(|_, w| {
+        w.moder8().output();
+        w.moder9().output();
+        w.moder10().output();
+        w.moder11().output();
+        w.moder12().output();
+        w.moder13().output();
+        w.moder14().output();
+        w.moder15().output()
+    });
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
