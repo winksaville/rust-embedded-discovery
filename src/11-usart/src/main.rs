3ebf4101c48cec965f18a23f99ba1787af6b07f0
Wink Saville <wink@saville.com>
2021-01-13 10:47:48 -0800

    uprintln macro
    Added itm to SerialPort for debug purposes and need "\r" instead of
    "\n" for EOL. Not sure why, yet :)

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,8 +2,42 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln, usart1};
+use aux11::{entry, iprint, iprintln, usart1, ITM};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    // For somereason my minicom needs "\r" for EOL
+    // and you need "linefeed ON". Use (Ctrl-A a)
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\r"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\r"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+    // Added itm so write_str could use iprintln!
+    itm: ITM,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        //iprintln!("write_str: s={}", s);
+        let _ = ws(self.usart1, s);
+        Ok(())
+    }
+}
+
 
 fn wc(usart: &mut usart1::RegisterBlock, ch: char) -> Result<(), &'static str> {
     // Wait for Transmit buffer Empty (TXE)
@@ -34,14 +68,15 @@ fn main() -> ! {
     let (usart1, mono_timer, mut itm) = aux11::init();
     iprintln!(&mut itm.stim[0], "Hi");
 
+    let mut serial = SerialPort { usart1, itm };
+
     let start = mono_timer.now();
-    // Send a single character
-    let _ = ws(usart1, "hello, world!\r");
+    uprintln!(serial, "hello, world!");
     let elapsed = start.elapsed();
 
 
     iprintln!(
-        &mut itm.stim[0],
+        &mut serial.itm.stim[0],
         "ws took {} ticks ({} us)",
         elapsed,
         ((elapsed as f32) / (mono_timer.frequency().0 as f32)) * 1e6
