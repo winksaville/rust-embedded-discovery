a2f24ebdeb344f5b2679985d19a6a8dbc53bbf27
Wink Saville <wink@saville.com>
2021-01-24 10:22:32 -0800

    Change 14-i2c to read 6 registers
    Also changed the code to loop and output all 6 register values plus the
    counter values. If things were working we should be able to pickup the
    STM32F3DISCOVERY board and see the values of the 6 output registers, but
    all we see are zeros :(

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -11,8 +11,8 @@ use aux14::{entry, iprint, iprintln, prelude::*};
 const MAGNETOMETER: u8 = 0b001_1110;
 
 // Addresses of the magnetometer's registers
-//const OUT_X_H_M: u8 = 0x03;
-const IRA_REG_M: u8 = 0x0A;
+const OUT_X_H_M: u8 = 0x03;
+//const IRA_REG_M: u8 = 0x0A;
 
 // Tweaks to "My Solution", but
 // returns 0x00 for IRA_REG_M :(
@@ -23,36 +23,37 @@ fn main() -> ! {
     //   Figure 309: page 842
     //    "Transfer sequence flowcart for I2C master transmitter for N<=255 bytes"
 
-    let (i2c1, _delay, mut itm) = aux14::init();
+    let (i2c1, mut delay, mut itm) = aux14::init();
 
-    let mut wait_txis_1_counter = 0;
-    let mut wait_txis_1 = || {
-        // Wait while txis is 0 so we can send more data
-        while i2c1.isr.read().txis().bit_is_clear() {
-            //wait_txis_1_counter = wait_txis_1_counter + 1;
-            wait_txis_1_counter += 1;
-        }
-    };
+    loop {
+        let mut wait_txis_1_counter = 0;
+        let mut wait_txis_1 = || {
+            // Wait while txis is 0 so we can send more data
+            while i2c1.isr.read().txis().bit_is_clear() {
+                //wait_txis_1_counter = wait_txis_1_counter + 1;
+                wait_txis_1_counter += 1;
+            }
+        };
 
-    let mut wait_tc_1_counter = 0;
-    let mut wait_tc_1 = || {
-        // Wait while register address is being sent
-        while i2c1.isr.read().tc().bit_is_clear() {
-            wait_tc_1_counter += 1;
-        }
-    };
+        let mut wait_tc_1_counter = 0;
+        let mut wait_tc_1 = || {
+            // Wait while register address is being sent
+            while i2c1.isr.read().tc().bit_is_clear() {
+                wait_tc_1_counter += 1;
+            }
+        };
 
-    let mut wait_rxne_1_counter = 0;
-    let mut wait_rxne_1 = || {
-        // Wait while register address is being sent
-        while i2c1.isr.read().rxne().bit_is_clear() {
-            wait_rxne_1_counter += 1;
-        }
-    };
+        let mut wait_rxne_1_counter = 0;
+        let mut wait_rxne_1 = || {
+            // Wait while register address is being sent
+            while i2c1.isr.read().rxne().bit_is_clear() {
+                wait_rxne_1_counter += 1;
+            }
+        };
 
-    // Stage 1: Send the address of the register we want to read to the
-    // magnetometer
-    {
+        // Stage 1: Send the address of the register we want to read to the
+        // magnetometer
+
         // Initialize i2c1.cr2
         // RefManual: 28.7.2
         // Send start with MAGNETOMETER address
@@ -65,36 +66,35 @@ fn main() -> ! {
         });
         wait_txis_1();
 
-        // Send the address of IRA_REG_M register in MAGNETOMETER we want to read
+        // Send the address of OUT_X_H_M register in MAGNETOMETER we want to read
         i2c1.txdr.write(|w| {
-            w.txdata().bits(IRA_REG_M)
+            w.txdata().bits(OUT_X_H_M)
         });
         wait_tc_1();
-    }
 
-    // Stage 2: Receive the contents of the register we asked for
-    let byte = {
+
+        // Stage 2: Receive the contents of the register we asked for
         // Broadcast RESTART with MAGNETOMETER address with the R/W bit set to Read
         i2c1.cr2.modify(|_, w| {
             w.start().set_bit();
             //w.sadd1().bits(MAGNETOMETER); // Since autoend was 0 above sadd1 unchanged?
             w.rd_wrn().set_bit();
-            w.nbytes().bits(1);
+            w.nbytes().bits(6);
             w.autoend().set_bit()
         });
-        wait_rxne_1();
 
-        // Read the data
-        i2c1.rxdr.read().rxdata().bits()
+        // Read the 6 bytes of data
+        let mut buffer = [0u8; 6];
+        for byte in &mut buffer {
+            wait_rxne_1();
+            *byte = i2c1.rxdr.read().rxdata().bits()
+        }
 
         // We don't need to explictly broadcast stop because AUTOEND is 1
-    };
+        iprintln!(&mut itm.stim[0], "buffer: {:?} txis={} tc={} rxne={}", buffer, wait_txis_1_counter, wait_tc_1_counter, wait_rxne_1_counter);
 
-    iprintln!(&mut itm.stim[0], "txis={} tc={} rxne={}", wait_txis_1_counter, wait_tc_1_counter, wait_rxne_1_counter);
-    // Expected output: 0x0A - 0b01001000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", IRA_REG_M, byte);
-
-    loop {}
+        delay.delay_ms(1_000_u16);
+    }
 }
 
 //// The original "My Solution"
