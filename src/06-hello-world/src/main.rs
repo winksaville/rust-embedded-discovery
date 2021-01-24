dd29929b44883ec44b63bb06b6b2f324f387e110
Wink Saville <wink@saville.com>
2021-01-24 09:48:40 -0800

    06-hello-world use  iprint and iprintln

--- src/06-hello-world/src/main.rs
+++ src/06-hello-world/src/main.rs
@@ -7,5 +7,15 @@ use aux6::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
+    let mut itm: aux6::ITM = aux6::init();
+
+    iprint!(&mut itm.stim[0], "iprint!: Hello, world!\n");
+    iprintln!(&mut itm.stim[0], "iprintln!: Hello, world!");
+
+    iprint!(&mut itm.stim[0], "iprint!: ");
+    iprint!(&mut itm.stim[0], "1");
+    iprint!(&mut itm.stim[0], "2");
+    iprint!(&mut itm.stim[0], "\n");
+
     panic!("Hello, world from panic!");
 }
